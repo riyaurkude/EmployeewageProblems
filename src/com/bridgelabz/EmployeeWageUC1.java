5747903ca951e9fd51cb6806415170e90a1cb06e
riyaurkude <urkude.riya@gmail.com>
2022-10-30 19:19:14 +0530

    Added: Added UC1

---
+++ src/com/bridgelabz/EmployeeWageUC1.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class EmployeeWageUC1 {
+
+	public static void main(String[] args)
+	{
+		//uc1
+		
+		//nt IS_FULL_TIME = 1; int EMP_RATE_PER_HOUR = 20;
+				int IS_FULL_TIME =1;
+				int EMP_RATE_PER_HOUR = 20;
+				//variables
+				int empHrs = 0;
+				int empWage = 0;
+				double empCheck = Math.floor(Math.random()*10)%2;
+				if (empCheck == IS_FULL_TIME)
+					empHrs = 8 ;
+				else
+				 	empHrs =0;
+				empWage = empHrs * EMP_RATE_PER_HOUR;
+				System.out.println("emp wage: "+empWage);
+
+	}
+
+}
