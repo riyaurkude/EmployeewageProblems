9fc33a2bbb7ed23c4b3c353992c5ccb4006ffb0f
riyaurkude <urkude.riya@gmail.com>
2022-10-30 19:43:05 +0530

    Added: Added UC4

--- src/com/bridgelabz/EmployeeWageUC1.java
+++ src/com/bridgelabz/EmployeeWageUC1.java
@@ -4,31 +4,36 @@ public class EmployeeWageUC1 {
 
 	public static void main(String[] args)
 	{
-		//uc3
+		//uc4
 		int IS_FULL_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
 		int IS_PART_TIME_HOUR = 8;
 		int IS_PART_TIME =0;
-		
-		int empHrs=0;
+		int workingdays=20;
 		int empWage=0;
 		
-		double empcheck=Math.floor(Math.random()*10)%2;
+		int empHrs = 0;
+		double employeeCheck = Math.floor(Math.random() * 10 % 2);
+		int i = (int)employeeCheck;
+		switch (i)
+		{
+			case 0:
+				empHrs =8;
+				break;
+			case 1:
+				empHrs =16;
+				break;
+			default:
+				empHrs = 0;
+				break;
+				}
 		
-		if(empcheck == IS_PART_TIME)
-		{
-			empHrs=8;
-			System.out.println("part-time hours:"+empHrs);
+				empWage = empHrs *  EMP_RATE_PER_HOUR;
+				System.out.println("The employee wage for "+empHrs+" is:" + empWage);
 		}
-		else
-		{
-			empHrs=16;
-			System.out.println("full-time hours:"+empHrs);
-		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("emp wage: "+ empWage);
-	}
+		
 
 }
+
 
 
